03c7f6b2003e452f42394203bee40c0b6315ad21
binhku2610 <binhku2610@gmail.com>
2024-03-13 10:17:59 +0700

    done2

--- tracker_client/app/src/main/java/com/anhquan/tracker_client/FMService.kt
+++
@@ -1,7 +0,0 @@
-package com.anhquan.tracker_client
-
-import android.annotation.SuppressLint
-import com.google.firebase.messaging.FirebaseMessagingService
-
-@SuppressLint("MissingFirebaseInstanceTokenRefresh")
-class FMService : FirebaseMessagingService()
